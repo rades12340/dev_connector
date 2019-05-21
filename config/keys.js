da6de2a0c2b9f03e4e5c2ddc228acfd8e704f1a1
rades12340 <rades12340@gmail.com>
2019-05-21 21:01:27 +0200

    register and login routes implemented

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://localhost/playground"
+  mongoURI: "mongodb://localhost/playground",
+  secretOrKey: "secret"
 };
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const bodyParser = require("body-parser");
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
+const passport = require("passport");
 
 const app = express();
 
 // db config
+
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const db = require("./config/keys").mongoURI;
 
 // Connect to mongodb
@@ -16,7 +22,11 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello"));
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
 
 // use routes
 app.use("/api/users", users);
